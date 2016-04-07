352954640b067a0072fd30f80bcb2434361c27fe
Stephan Bösebeck <stephan.boesebeck@holidayinsider.com>
2016-04-07 16:12:45 +0200

    bugfix

--- src/main/java/de/caluga/ergodox/macros/MacroAction.java
+++ src/main/java/de/caluga/ergodox/macros/MacroAction.java
@@ -31,10 +31,6 @@ public class MacroAction {
         this.wait = wait;
     }
 
-    public void setAction(Action action) {
-        this.action = action;
-    }
-
     public Action getAction() {
         return action;
     }
